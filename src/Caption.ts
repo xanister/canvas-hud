65af3abe4761c5a253a323d58522d47af5e95d7e
Nicholas Frees <xanister@inkintosh.local>
2017-12-10 22:05:04 -0500

    incremental

--- src/Caption.ts
+++ src/Caption.ts
@@ -1,8 +1,29 @@
+// export type CaptionMessage = string;
+// export type CaptionMessageResolver = () => string;
+
+export interface CaptionOptions {
+    // message: CaptionMessage | CaptionMessageResolver;
+    context?: any;
+    message: (context?: any) => string;
+    x: number;
+    y: number;
+}
+
 export class Caption {
+    message: (context: any) => string;
+    context: any;
     x: number;
     y: number;
 
-    render(context: CanvasRenderingContext2D) {
+    constructor(options: CaptionOptions) {
+        this.message = options.message;
+        this.context = options.context || this;
+        this.x = options.x;
+        this.y = options.y;
+    }
 
+    render(context: CanvasRenderingContext2D) {
+        context.font = "30px Arial";
+        context.fillText(this.message(this.context), this.x, this.y);
     }
 }
